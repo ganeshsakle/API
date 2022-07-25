e82b25f77b0a91b37e4de59ad976c26a15127c3b
ganeshsakle <ganeshsakle7@gmail.com>
2022-07-25 13:24:47 +0530

    Api practices

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+    render json: Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessible_entity
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
